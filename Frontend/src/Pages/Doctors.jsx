d0ad3ea0ecb499e6e4ba3a074f1d499d8dc6b93b
VivekDahiya06 <vivekdahiya312@gmail.com>
2025-01-31 19:40:03 +0530

    Add No doctors image

--- Frontend/src/Pages/Doctors.jsx
+++ Frontend/src/Pages/Doctors.jsx
@@ -6,7 +6,8 @@ import { AnimatePresence, motion } from 'motion/react'
 import BackdropModal from '../components/BackdropModal'
 import Header from '../components/Header'
 import { GrDocumentUpdate } from "react-icons/gr";
-import { AppContext } from '../Store/Context'
+import { AppContext } from '../Store/Context';
+import No_Doctors_Found from '../assets/Images/No_Doctors_Found.png';
 
 const Doctors = () => {
 
@@ -116,155 +117,183 @@ const Doctors = () => {
 
   return (
     <>
-      
+
       <Header />
 
-      <div className={styles.mainContainer}>
-        <div className={styles.Container}>
-          {
-            doctor_Data.map((doctor, index) => (
-              <Doctor_Card key={index} doctor={doctor} index={index} setDeleteIndex={setDeleteIndex} />
-            ))
-          }
-        </div>
-        <MotionButton
-          whileHover={{ scale: 1.1 }}
-          variant="contained"
-          sx={{
-            fontSize: "1.2rem",
-            width: "5em",
-            margin: "0.8em 0",
-            "@media screen and (width <= 520px)": {
-              width: "70%",
-            }
-          }}
-          onClick={openAndCloseDoctorForm}
-        >
-          Add
-        </MotionButton>
+      {
+        doctor_Data.length === 0 ? (
+          <div className={styles.NotFoundContainer}>
+            <div className={styles.NotFoundImageContainer}>
+              <h1>No Doctors Found</h1>
+              <img src={No_Doctors_Found} alt='No Doctors' style={{width:"50%",height:"50%"}} />
+            </div>
+            <MotionButton
+              whileHover={{ scale: 1.1 }}
+              variant="contained"
+              sx={{
+                fontSize: "1.2rem",
+                width: "5em",
+                margin: "0.8em 0",
+                "@media screen and (width <= 520px)": {
+                  width: "70%",
+                }
+              }}
+              onClick={openAndCloseDoctorForm}
+            >
+              Add
+            </MotionButton>
+          </div>
+        ) : (
+          <div className={styles.mainContainer}>
+            <div className={styles.Container}>
+              {
+                doctor_Data.map((doctor, index) => (
+                  <Doctor_Card key={index} doctor={doctor} index={index} setDeleteIndex={setDeleteIndex} />
+                ))
+              }
+            </div>
+            <MotionButton
+              whileHover={{ scale: 1.1 }}
+              variant="contained"
+              sx={{
+                fontSize: "1.2rem",
+                width: "5em",
+                margin: "0.8em 0",
+                "@media screen and (width <= 520px)": {
+                  width: "70%",
+                }
+              }}
+              onClick={openAndCloseDoctorForm}
+            >
+              Add
+            </MotionButton>
 
-        {/* Backdrop Modal for adding new card */}
-        <AnimatePresence>
-          {
-            formOpen && (
-              <BackdropModal modalHandler={openAndCloseDoctorForm}>
+            {/* Backdrop Modal for adding new card */}
+            <AnimatePresence>
+              {
+                formOpen && (
+                  <BackdropModal modalHandler={openAndCloseDoctorForm}>
 
-                {/* Form */}
-                <form className={styles.form} onSubmit={formHandler}>
-                  <h1>Add Doctor</h1>
+                    {/* Form */}
+                    <form className={styles.form} onSubmit={formHandler}>
+                      <h1>Add Doctor</h1>
 
-                  {/* Form Container having Info Container and Image Upload Container */}
-                  <div className={styles.formContainer}>
+                      {/* Form Container having Info Container and Image Upload Container */}
+                      <div className={styles.formContainer}>
 
-                    {/* Info Container having all the info */}
-                    <section className={styles.infoContainer}>
-                      <TextField className={styles.input} label="Name" name='name' color='primary' onChange={handleInputChange} />
-                      <TextField className={styles.input} label="Experience" name='experience' color='primary' onChange={handleInputChange} />
-                      <div className={styles.radioContainer}>
-                        <FormLabel>Gender</FormLabel>
-                        <RadioGroup row name='gender' onChange={handleInputChange} sx={{ '@media (max-width: 520px)': { flexDirection: 'column' } }}>
-                          <FormControlLabel label="Male" value='male' control={<Radio />} />
-                          <FormControlLabel label="Female" value='female' control={<Radio />} />
-                        </RadioGroup>
+                        {/* Info Container having all the info */}
+                        <section className={styles.infoContainer}>
+                          <TextField className={styles.input} label="Name" name='name' color='primary' onChange={handleInputChange} />
+                          <TextField className={styles.input} label="Experience" name='experience' color='primary' onChange={handleInputChange} />
+                          <div className={styles.radioContainer}>
+                            <FormLabel>Gender</FormLabel>
+                            <RadioGroup row name='gender' onChange={handleInputChange} sx={{ '@media (max-width: 520px)': { flexDirection: 'column' } }}>
+                              <FormControlLabel label="Male" value='male' control={<Radio />} />
+                              <FormControlLabel label="Female" value='female' control={<Radio />} />
+                            </RadioGroup>
+                          </div>
+                          <TextField className={styles.input} label="Specialization" name='specialization' color='primary' onChange={handleInputChange} />
+                          <TextField className={styles.input} label="Location" name='location' color='primary' onChange={handleInputChange} />
+                        </section>
+
+                        {/* Image Upload */}
+                        <section className={styles.imageUpload}>
+                          <div className={styles.imageUploadContainer}>
+                            <label htmlFor='image' className={styles.imageUploadLabel}>
+                              <GrDocumentUpdate style={{ width: '100%', height: '100%' }} />
+                              <span>Upload Image</span>
+                            </label>
+                            <input
+                              id='image'
+                              type='file'
+                              name='image'
+                              onChange={handleInputChange}
+                              style={{ display: 'none' }}
+                            />
+                          </div>
+                        </section>
                       </div>
-                      <TextField className={styles.input} label="Specialization" name='specialization' color='primary' onChange={handleInputChange} />
-                      <TextField className={styles.input} label="Location" name='location' color='primary' onChange={handleInputChange} />
-                    </section>
 
-                    {/* Image Upload */}
-                    <section className={styles.imageUpload}>
-                      <div className={styles.imageUploadContainer}>
-                        <label htmlFor='image' className={styles.imageUploadLabel}>
-                          <GrDocumentUpdate style={{ width: '100%', height: '100%' }} />
-                          <span>Upload Image</span>
-                        </label>
-                        <input
-                          id='image'
-                          type='file'
-                          name='image'
-                          onChange={handleInputChange}
-                          style={{ display: 'none' }}
-                        />
-                      </div>
-                    </section>
-                  </div>
+                      {/* Submit Button */}
+                      <MotionButton
+                        whileHover={{ scale: 1.08 }}
+                        sx={{
+                          margin: '1.2em 0',
+                        }}
+                        variant='outlined'
+                        type='submit'>
+                        Submit
+                      </MotionButton>
 
-                  {/* Submit Button */}
-                  <MotionButton
-                    whileHover={{ scale: 1.08 }}
-                    sx={{
-                      margin: '1.2em 0',
-                    }}
-                    variant='outlined'
-                    type='submit'>
-                    Submit
-                  </MotionButton>
+                    </form>
 
-                </form>
-
-              </BackdropModal>
-            )
-          }
-        </AnimatePresence>
+                  </BackdropModal>
+                )
+              }
+            </AnimatePresence>
 
 
-        {/* Alert Message for Error */}
-        <Snackbar
-          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-          open={alertOpen}
-          autoHideDuration={2000}
-          onClose={() => setAlertOpen(false)}
-        >
-          <Alert
-            onClose={() => setAlertOpen(false)}
-            severity='error'
-            variant='filled'
-          >
-            {alertMessage}
-          </Alert>
-        </Snackbar>
-
-        
-        {/* Delete Backdrop opened on click of Delete Button */}
-          <Dialog
-            open={deleteAlert}
-        >
-          
-            <DialogTitle sx={{
-              fontSize: '2rem',
-              fontWeight: 'bolder'
-            }}>
-              Are You Sure !!
-            </DialogTitle>
-
-            <DialogContent sx={{
-              display: 'flex',
-              justifyContent: 'space-evenly'
-            }}>
-              <Button
-                variant='outlined'
-                color='warning'
-                onClick={() => {
-                  doctor_Data.splice(deleteIndex, 1);
-                  setDeleteAlert(false);
-                }}>
-                Yes
-              </Button>
-              <Button
-                variant='outlined'
-                color='warning'
-                onClick={() => { setDeleteAlert(false) }}>
-                No
-              </Button>
-            </DialogContent>
-
-          </Dialog>
+            {/* Alert Message for Error */}
+            <Snackbar
+              anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+              open={alertOpen}
+              autoHideDuration={2000}
+              onClose={() => setAlertOpen(false)}
+            >
+              <Alert
+                onClose={() => setAlertOpen(false)}
+                severity='error'
+                variant='filled'
+              >
+                {alertMessage}
+              </Alert>
+            </Snackbar>
 
 
-        {/* Edit Backdrop form opened on click of Edit Button */}
+            {/* Delete Backdrop opened on click of Delete Button */}
+            <Dialog
+              open={deleteAlert}
+            >
+
+              <DialogTitle sx={{
+                fontSize: '2rem',
+                fontWeight: 'bolder'
+              }}>
+                Are You Sure ?
+              </DialogTitle>
+
+              <DialogContent sx={{
+                display: 'flex',
+                justifyContent: 'space-evenly'
+              }}>
+                <Button
+                  variant='outlined'
+                  color='warning'
+                  onClick={() => {
+                    doctor_Data.splice(deleteIndex, 1);
+                    setDeleteAlert(false);
+                  }}>
+                  Yes
+                </Button>
+                <Button
+                  variant='outlined'
+                  color='warning'
+                  onClick={() => { setDeleteAlert(false) }}>
+                  No
+                </Button>
+              </DialogContent>
+
+            </Dialog>
 
 
-      </div>
+            {/* Edit Backdrop form opened on click of Edit Button */}
+
+
+          </div>
+
+        )
+      }
+
     </>
   )
 }
