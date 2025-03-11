8b33b88eae99b3a4548f7c41cdbce88a6c1da194
VivekDahiya06 <vivekdahiya312@gmail.com>
2025-03-11 14:56:58 +0530

    Added a heading to home page

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -12,23 +12,30 @@ const Home = () => {
     { name: "Doctors", Link: "doctors" },
     { name: "Patients", Link: "patients" },
   ];
-  
+
   return (
-    <div className={styles.homeContainer}>
-      {pages.map((element, index) => (
-        <motion.div
-          key={index}
-          className={styles.card}
-          whileHover={{
-            scale: 1.1,
-            transition: { duration: 0.3 },
-          }}
-          onClick={() => navigate(element.Link)}
-        >
-          <h1 className={styles.h4}>{element.name}</h1>
-        </motion.div>
-      ))}
-    </div>
+    <>
+      <div className={styles.heading}>
+        <h1>Welcome to the Health & Wealth HMS</h1>
+      </div>
+
+      <div className={styles.homeContainer}>
+        {pages.map((element, index) => (
+          <motion.div
+            key={index}
+            className={styles.card}
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.31 },
+            }}
+            onClick={() => navigate(element.Link)}
+          >
+            <h1 className={styles.h4}>{element.name}</h1>
+          </motion.div>
+        ))}
+      </div>
+    </>
+
   );
 };
 
